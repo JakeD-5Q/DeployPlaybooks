32132f27c46d5aa562e8649b4efc9a505c7e0aa6
Jake Derkowski <77984636+JakeD-5Q@users.noreply.github.com>
2022-06-15 12:27:42 -0500

    writing deploy script

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # DeployPlaybooks
 Deploy these playbooks to all new workspaces
+
+
+### Escalate Incident
+
+
+
+### Get-GeoIPdata-tagIncident
+
+
+### Send-email-with-formatted-report
+
+
+### Start-CompleteMDEInvestigation
+
+
+### VirusTotalEnrichAllEntities
---
+++ test.ps1
@@ -0,0 +1,14 @@
+$EnrichEmail = 'Enrich-email-plus\azuredeploy.json'
+$EnrichEmailParams = 'Enrich-email-plus\azuredeploy.parameters.json'
+
+$GetGeoIP = 'Get-GEOIPdata-tagIncident\azuredeploy.json'
+$GetGeoIPParams = 'Get-GEOIPdata-tagIncident\azuredeploy.parameters.json'
+
+$NewIncidentEmail = 'Send-email-with-formatted-incident-report\azuredeploy.json'
+$NewIncidentEmailParams = 'Send-email-with-formatted-incident-report\azuredeploy.parameters.json'
+
+$FullInvestgation = 'Start-CompleteMDEInvestigation\azuredeploy.json'
+$FullInvestgationParams = 'Start-CompleteMDEInvestigation\azuredeploy.parameters.json'
+
+$VTEnrichAllEntities = 'VirusTotalEnrichAllEntites\azuredeploy.json'
+$VTEnrichAllEntitiesParams = 'VirusTotalEnrichAllEntites\azuredeploy.parameters.json'
